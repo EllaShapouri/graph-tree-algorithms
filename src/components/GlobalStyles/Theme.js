dc7b00eea8cf30e647c228632cee26f622409fa8
EllaShapouri <ellashapouri@gmail.com>
2022-05-10 18:16:53 +0430

    add global styles and assets, install styled-component and react-router

---
+++ src/components/GlobalStyles/Theme.js
@@ -0,0 +1,11 @@
+const theme = {
+    colors: {
+        primary: "#742BFA",
+        secondary: "#9A47E9",
+        text: "#2E2E2E",
+        text2: "#5B5353",
+        white: "#eee",
+    },
+};
+
+export default theme;
