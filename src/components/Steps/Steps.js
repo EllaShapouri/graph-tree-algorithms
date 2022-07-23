2d82e82fb57b054e9e88e882111eb8d2ea3def7f
EllaShapouri <ellashapouri@gmail.com>
2022-07-23 18:11:24 +0430

    Change the order of steps

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -1,13 +1,27 @@
 import {Gradientbox} from "../Common/Gradientbox";
 import {Paragraph} from "../Common/Paragraph";
 import {Title} from "../Common/Title";
-import { StepItem, StepsWrapper } from "./steps.styled";
+import { GuideLine,GuideLine2, StepItem, StepsWrapper } from "./steps.styled";
 
 import { ReactComponent as GraphShape } from "./../../assets/images/graph.svg";
+import Guide from "./../../assets/images/guide-line.svg";
 
 const Steps = () => {
     return (
-        <StepsWrapper className="steps-wrapper">
+        <StepsWrapper>
+            <StepItem>
+                <Gradientbox width="90px" height="90px">
+                    <GraphShape />
+                </Gradientbox>
+                <Title maxWidth="10em">Chose your Algorithm and data structure</Title>
+                <Paragraph fontSize=".9em">
+                    It is a long established fact that a reader will be
+                    distracted
+                </Paragraph>
+            </StepItem>
+
+            <GuideLine src={Guide} alt="" />
+
             <StepItem>
                 <Gradientbox width="90px" height="90px">
                     <GraphShape />
@@ -22,16 +36,7 @@ const Steps = () => {
                 </Paragraph>
             </StepItem>
 
-            <StepItem>
-                <Gradientbox width="90px" height="90px">
-                    <GraphShape />
-                </Gradientbox>
-                <Title>Chose your Algorithm</Title>
-                <Paragraph fontSize=".9em">
-                    It is a long established fact that a reader will be
-                    distracted
-                </Paragraph>
-            </StepItem>
+            <GuideLine2 src={Guide} alt="" />
 
             <StepItem>
                 <Gradientbox width="90px" height="90px">
